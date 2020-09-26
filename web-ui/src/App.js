d73de6dbc11612fd0f4a8dbd07e7caff214d68c9
17543268 <Mosin.M.Yu@sberbank.ru>
2020-09-26 23:35:22 +0300

    added simple react hook with dummy list

--- web-ui/src/App.js
+++ web-ui/src/App.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import TodoList from './Todo/TodoList'
 
 
 function App() {
-  return <div/>
+  return (
+      <div className='wrapper'>
+        <h1>React tutorial</h1>
+          <TodoList/>
+      </div>
+  )
 }
 
 export default App;
