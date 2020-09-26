74ce79b38e41c6292860ec83a52273e609652e55
17543268 <Mosin.M.Yu@sberbank.ru>
2020-09-26 23:38:43 +0300

    added css-styles in same file using const

--- web-ui/src/Todo/TodoList.js
+++ web-ui/src/Todo/TodoList.js
@@ -1,9 +1,17 @@
 import React from 'react'
 
+const styles = {
+    ul: {
+        listStyle: 'none',
+        margin: 0,
+        padding: 0
+    }
+}
+
 export default function TodoList(){
 
     return (
-    <ul>
+    <ul styles={styles.ul}>
         <li>1</li>
         <li>2</li>
     </ul>
